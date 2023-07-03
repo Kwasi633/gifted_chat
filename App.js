5564d993ab72ed1012dcd91844c3b44de97a8a2c
Ezra Asiedu <ezrakwasi633@gmail.com>
2023-07-02 20:35:41 -0700

    Add files

---
+++ App.js
@@ -0,0 +1,135 @@
+import React from 'react';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons'; // Updated import statement
+import styles from './src/styles/appStyles';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>VECTOR ICONS</Text>
+
+      <ScrollView>
+        <View style={styles.iconRow}>
+          <Icon name="person" size={50} color="red" />
+          <Icon name="face" size={50} color="red" />
+          <Icon name="account-circle" size={50} color="red" />
+          <Icon name="insert-emoticon" size={50} color="red" />
+        </View>
+
+        <View style={styles.iconRow}>
+          <Icon name="movie" size={50} color="red" />
+          <Icon name="play-arrow" size={50} color="red" />
+          <Icon name="pause-circle-filled" size={50} color="red" />
+          <Icon name="save" size={50} color="red" />
+        </View>
+
+        <View style={styles.iconRow}>
+          <Icon name="tag" size={50} color="red" />
+          <Icon name="share" size={50} color="red" />
+          <Icon name="save-alt" size={50} color="red" />
+          <Icon name="qr-code" size={50} color="red" />
+        </View>
+
+        <View style={styles.iconRow}>
+          <Icon name="contacts" size={50} color="red" />
+          <Icon name="fast-forward" size={50} color="red" />
+          <Icon name="group" size={50} color="red" />
+          <Icon name="fast-rewind" size={50} color="red" />
+        </View>
+
+        <View style={styles.iconRow}>
+          <Icon name="cancel" size={50} color="red" />
+          <Icon name="arrow-forward" size={50} color="red" />
+          <Icon name="done" size={50} color="red" />
+          <Icon name="close" size={50} color="red" />
+        </View>
+
+        <View style={styles.iconRow}>
+          <Icon name="person" size={50} color="red" />
+          <Icon name="face" size={50} color="red" />
+          <Icon name="account-circle" size={50} color="red" />
+          <Icon name="insert-emoticon" size={50} color="red" />
+        </View>
+
+        <View style={styles.iconRow}>
+          <Icon name="movie" size={50} color="red" />
+          <Icon name="play-arrow" size={50} color="red" />
+          <Icon name="pause-circle-filled" size={50} color="red" />
+          <Icon name="save" size={50} color="red" />
+        </View>
+
+        <View style={styles.iconRow}>
+          <Icon name="tag" size={50} color="red" />
+          <Icon name="share" size={50} color="red" />
+          <Icon name="save-alt" size={50} color="red" />
+          <Icon name="qr-code" size={50} color="red" />
+        </View>
+
+        <View style={styles.iconRow}>
+          <Icon name="contacts" size={50} color="red" />
+          <Icon name="fast-forward" size={50} color="red" />
+          <Icon name="group" size={50} color="red" />
+          <Icon name="fast-rewind" size={50} color="red" />
+        </View>
+
+        <View style={styles.iconRow}>
+          <Icon name="cancel" size={50} color="red" />
+          <Icon name="arrow-forward" size={50} color="red" />
+          <Icon name="done" size={50} color="red" />
+          <Icon name="close" size={50} color="red" />
+        </View>
+
+        <View style={styles.iconRow}>
+          <Icon name="person" size={50} color="red" />
+          <Icon name="face" size={50} color="red" />
+          <Icon name="account-circle" size={50} color="red" />
+          <Icon name="insert-emoticon" size={50} color="red" />
+        </View>
+
+        <View style={styles.iconRow}>
+          <Icon name="movie" size={50} color="red" />
+          <Icon name="play-arrow" size={50} color="red" />
+          <Icon name="pause-circle-filled" size={50} color="red" />
+          <Icon name="save" size={50} color="red" />
+        </View>
+
+        <View style={styles.iconRow}>
+          <Icon name="tag" size={50} color="red" />
+          <Icon name="share" size={50} color="red" />
+          <Icon name="save-alt" size={50} color="red" />
+          <Icon name="qr-code" size={50} color="red" />
+        </View>
+
+        <View style={styles.iconRow}>
+          <Icon name="contacts" size={50} color="red" />
+          <Icon name="fast-forward" size={50} color="red" />
+          <Icon name="group" size={50} color="red" />
+          <Icon name="fast-rewind" size={50} color="red" />
+        </View>
+
+        <View style={styles.iconRow}>
+          <Icon name="cancel" size={50} color="red" />
+          <Icon name="arrow-forward" size={50} color="red" />
+          <Icon name="done" size={50} color="red" />
+          <Icon name="close" size={50} color="red" />
+        </View>
+
+        <View style={styles.iconRow}>
+          <Icon name="contacts" size={50} color="red" />
+          <Icon name="fast-forward" size={50} color="red" />
+          <Icon name="group" size={50} color="red" />
+          <Icon name="fast-rewind" size={50} color="red" />
+        </View>
+
+        <View style={styles.iconRow}>
+          <Icon name="cancel" size={50} color="red" />
+          <Icon name="arrow-forward" size={50} color="red" />
+          <Icon name="done" size={50} color="red" />
+          <Icon name="close" size={50} color="red" />
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default App;
